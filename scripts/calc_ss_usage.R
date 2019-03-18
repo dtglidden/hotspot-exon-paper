9ef2988be80aa67f9e17b63c10355e20d93b1ad4
David Glidden <dtglidden@gmail.com>
2019-03-18 13:29:30 -0400

    Cosmetic change

--- scripts/calc_ss_usage.R
+++ scripts/calc_ss_usage.R
@@ -3,7 +3,9 @@
 ## NB: Because sequencing experiments vary in their coverage, not all exons
 ## may have their splice sites scored, and some might only have one splice site scored
 
-source(file.path("..", "lib", "feature_gen.R"))
+suppressPackageStartupMessages({
+  source(file.path("..", "lib", "feature_gen.R"))
+})
 seqFile <- file.path(Sys.getenv("HOME"), "scratch", "amiloride", "star", "ctrl", "SJ.out.tab")
 txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
 exs <- exons(txdb)
