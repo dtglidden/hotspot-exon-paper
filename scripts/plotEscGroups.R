73373d0e04671464717dd011b7650fdd23b0a2ed
David Glidden <dtglidden@gmail.com>
2019-03-21 13:48:17 -0400

    Figure 6B script now plots all groups together

---
+++ scripts/plotEscGroups.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+suppressPackageStartupMessages({
+  library(ggplot2)
+})
+
+df <- read.delim(file.path("..", "data", "see-saw.txt"))
+
+ggplot(df, aes(v_exon_eff, skip.incl, color=group)) +
+  geom_point(size=2.5) +
+  labs(x="Splicing Efficiency of flanking exon",
+       y="Skipping:Inclusion ratio of test exon",
+       color="") +
+  theme(plot.title=element_text(hjust=0.5),
+        axis.text.x=element_text(color="black"),
+        axis.text.y=element_text(color="black"),
+        text=element_text(size=18),
+        panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.background=element_blank(),
+        panel.border=element_rect(fill=NA)) +
+  stat_smooth(method="lm", se=F)
+ggsave(file.path("..", "plots", "see-saw_with_groups.pdf"), width=6, height=5, useDingbats=F)
