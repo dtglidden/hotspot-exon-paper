11f8c32032e216f89290015c7504366b7e2c1781
David Glidden <dtglidden@gmail.com>
2019-10-17 14:03:26 -0400

    Updating feature correlation matrix plot

--- scripts/wt_spleff_correlations.R
+++ scripts/wt_spleff_correlations.R
@@ -5,67 +5,41 @@
 ## SS5 and SS3 Maxent scores
 ## SS5score - flanking SS3score; SS3score - flanking SS5score
 suppressPackageStartupMessages({
-source(file.path("..", "lib", "feature_query.R"))
-source(file.path("..", "lib", "feature_gen.R"))
-library(corrplot)
+  source(file.path("..", "lib", "feature_query.R"), chdir=T)
+  source(file.path("..", "lib", "feature_gen.R"), chdir=T)
+  library(corrplot)
 })
 
-exs <- exons(TxDb.Hsapiens.UCSC.hg19.knownGene)
-mutData <- read.delim(file.path("..", "data", "input_ML_complete_table.txt"))
-sjFile <- file.path("~", "scratch", "amiloride", "star", "ctrl", "SJ.out.tab")
-mapsyExons <- GRanges(seqnames=mutData$chrom_exon,
-                      ranges=IRanges(start=mutData$start_exon, end=mutData$end_exon),
-                      strand=mutData$strand,
-                      seqinfo=Seqinfo(genome="hg19"),
-                      vivo_ref_spliced=mutData$vivo_ref_spliced,
-                      vivo_ref_unspliced=mutData$vivo_ref_unspliced,
-                      vivo_alt_spliced=mutData$vivo_alt_spliced,
-                      vivo_alt_unspliced=mutData$vivo_alt_unspliced)
-## Remove duplicates because this dataset has entries for each mutation, which may be in the same exon
-## We only care about the WT exons
+mapsyExons <- readRDS(file.path("..", "data", "mapsy_features_gr.rds"))
 mapsyExons <- mapsyExons[!duplicated(ranges(mapsyExons))]
 
-ov <- findOverlaps(mapsyExons, exs, type="equal")
-ids <- mcols(exs)[subjectHits(ov), "exon_id"]
-mapsyExons <- mapsyExons[queryHits(ov)]
-mapsyExons$exon_id <- ids
-
-mapsyExons$splEff <- MapsySplicingEfficiency(mapsyExons$vivo_ref_spliced, mapsyExons$vivo_ref_unspliced)
+mapsyExons$splEff <- MapsySplicingEfficiency(mapsyExons$hek_ws, mapsyExons$hek_wu)
 mapsyExons$alSkew <- AllelicSkew(
-  mapsyExons$vivo_ref_spliced,
-  mapsyExons$vivo_ref_unspliced,
-  mapsyExons$vivo_alt_spliced,
-  mapsyExons$vivo_alt_unspliced)
-
+  mapsyExons$hek_ws,
+  mapsyExons$hek_wu,
+  mapsyExons$hek_ms,
+  mapsyExons$hek_mu)
 
 exonData <- QueryExonsWithSSUsage()
 mapsyExons <- AddMcols(mapsyExons, exonData)
-#mapsyExons$ssScoreSum <- mapsyExons$ss5score + mapsyExons$ss3score
-#mapsyExons$ssScoreDiff <- mapsyExons$ss5score - mapsyExons$ss3score
-#mapsyExons$ssScoreAbsDiff <- abs(mapsyExons$ss5score - mapsyExons$ss3score)
-#mapsyExons$ssUsageSum <- mapsyExons$ss5usage + mapsyExons$ss3usage
-#mapsyExons$ssUsageDiff <- mapsyExons$ss5usage - mapsyExons$ss3usage
-#mapsyExons$ssUsageAbsDiff <- abs(mapsyExons$ss5usage - mapsyExons$ss3usage)
 
 
 ## Plot the correlation matrix
 featureNames <- c(
-  "M/W Ratio"="alSkew",
+  "Allelic Imbalance"="alSkew",
   "Splicing Efficiency"="splEff",
-  "WT Spliced Counts"="vivo_ref_spliced",
-  "WT Unspliced Counts"="vivo_ref_unspliced",
+  "WT Spliced Counts"="hek_ws",
+  "WT Unspliced Counts"="hek_wu",
   "5'SS Maxent Score"="ss5score",
   "3'SS Maxent Score"="ss3score",
-#  "Maxent Score Sum"="ssScoreSum",
-#  "Maxent Score Diff"="ssScoreDiff",
-#  "Maxent Score Abs Diff"="ssScoreAbsDiff",
   "5'SS Usage"="ss5usage",
   "3'SS Usage"="ss3usage",
-#  "SS Usage Sum"="ssUsageSum",
-#  "SS Usage Diff"="ssUsageDiff",
-#  "SS Usage Abs Diff"="ssUsageAbsDiff",
-  "ESE Density"="chasin_ese_density",
-  "ESS Density"="chasin_ess_density"
+  "Avg EI"="ei",
+  "Avg Delta EI"="avgDeltaEIScores",
+  "Avg Delta A3SS"="avgDeltaA3SSScores",
+  "Avg Delta A5SS"="avgDeltaA5SSScores",
+  "# Exons"="nExons",
+  "Gene Length"="geneLength"
 )
 
 corMat <- cor(as.data.frame(mcols(mapsyExons)[, featureNames]))
