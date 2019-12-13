e375ba80d67e9784b4f1fb1c4f5fa79ad9718b4e
David Glidden <dtglidden@gmail.com>
2019-12-13 14:45:32 -0500

    Figure 5B works with example data and has t-tests within the script

--- scripts/compare_ss_usage_and_hgmd_splice.R
+++ scripts/compare_ss_usage_and_hgmd_splice.R
@@ -1,26 +1,44 @@
 #!/usr/bin/env Rscript
 suppressPackageStartupMessages({
-  source(file.path("..", "lib", "feature_gen.R"))
-  source(file.path("..", "lib", "granges_util.R"))
+  source(file.path("..", "lib", "feature_gen.R"), chdir=T)
+  source(file.path("..", "lib", "granges_util.R"), chdir=T)
+  library(getopt)
   library(sqldf)
   library(ggplot2)
   library(ggsignif)
   library(dplyr)
 })
 
+opt <- getopt(matrix(c("test", "t", 0, "logical"),
+                     byrow=T, ncol=4))
+if (is.null(opt$test)) opt$test <- F
+
 set.seed(1)
 
-exsWithSSUsage <- QueryExonsWithSSUsage(as.GRanges=T)
+exsWithSSUsage <- if (opt$test) {
+  readRDS(file.path("..", "example_data", "exons_with_ss_usage.rds"))
+} else {
+  QueryExonsWithSSUsage(as.GRanges=T)
+}
+
 ## Add complete splice site regions to each exon
 exsWithSSUsageAndSites <- AddSpliceSites(exsWithSSUsage)
 
 ## Get HGMD variants with associated SS usage values
-hgmdGr <- Df2Gr(read.delim(file.path("..", "data", "hgmd_splice.txt"), colClasses=c("acc_num"="character")))
+hgmdGr <- if (opt$test) {
+  Df2Gr(read.delim(file.path("..", "example_data", "splice_alleles_test.txt")))
+} else {
+  Df2Gr(read.delim(file.path("..", "data", "hgmd_splice.txt"), colClasses=c("acc_num"="character")))
+}
 hgmdMerge <- mergeByOverlaps(exsWithSSUsageAndSites, hgmdGr)
 hgmdMerge <- hgmdMerge[!duplicated(hgmdMerge$hgmdGr), ]
 
 ## Get 'control' variants (HGMD variants within the critical GT/AG nucleotides)
-hgmdCritGr <- Df2Gr(read.delim(file.path("..", "data", "hgmd_splice_critical.txt"), colClasses=c("acc_num"="character")))
+hgmdCritGr <- if (opt$test) {
+  Df2Gr(read.delim(file.path("..", "example_data", "splice_critical_alleles_test.txt")))
+} else {
+  Df2Gr(read.delim(file.path("..", "data", "hgmd_splice_critical.txt"), colClasses=c("acc_num"="character")))
+}
 hgmdCritMerge <- mergeByOverlaps(exsWithSSUsageAndSites, hgmdCritGr)
 hgmdCritMerge <- hgmdCritMerge[!duplicated(hgmdCritMerge$hgmdCritGr), ]
 
@@ -35,8 +53,24 @@ avgSS3Usage <- mean(hgmdMerge$ss3usage)
 avgCritSS5Usage <- mean(hgmdCritMerge$ss5usage)
 avgCritSS3Usage <- mean(hgmdCritMerge$ss3usage)
 
+## t-tests between HGMD variants and RBP knock-downs
+## 'Func_num' > 0 => Sensitive; 'Func_num == 0 => Resistant
+rbpUsage <- if (opt$test) {
+  read.delim(file.path("..", "example_data", "HepG2_exons_by_func_rbp_num_usage.txt"))
+} else {
+  read.delim(file.path("..", "data", "HepG2_exons_by_func_rbp_num_usage.txt"))
+}
+tSens5SS <- t.test(hgmdMerge$ss5usage, rbpUsage$Fivep_usage[rbpUsage$Func_num > 0])
+tRes5SS <- t.test(hgmdCritMerge$ss5usage, rbpUsage$Fivep_usage[rbpUsage$Func_num == 0])
+tSens3SS <- t.test(hgmdMerge$ss3usage, rbpUsage$Threep_usage[rbpUsage$Func_num > 0])
+tRes3SS <- t.test(hgmdCritMerge$ss3usage, rbpUsage$Threep_usage[rbpUsage$Func_num == 0])
+
 ## Incorporate RBP binding usage data
-rbpUsage <- read.delim(file.path("..", "data", "ss_usage_by_binding.txt"), col.names=c("grp", "meanUsage", "seUsage"))
+rbpUsage <- if (opt$test) {
+  read.delim(file.path("..", "example_data", "ss_usage_by_binding.txt"), col.names=c("grp", "meanUsage", "seUsage"))
+} else {
+  read.delim(file.path("..", "data", "ss_usage_by_binding.txt"), col.names=c("grp", "meanUsage", "seUsage"))
+}
 rbpUsage$meanUsage <- rbpUsage$meanUsage / 100
 rbpUsage$seUsage <- rbpUsage$seUsage / 100
 rbpUsage$grp <- c("Resistant 3'SS", #3'ss
@@ -63,10 +97,11 @@ dfSummary <- boxDf %>% group_by(grp) %>%
 dfSummary$grp <- factor(dfSummary$grp, levels=as.character(dfSummary$grp))
 halfNRow <- nrow(dfSummary) / 2
 dfSummary$ss <- factor(c(rep("5'SS", halfNRow), rep("3'SS", halfNRow)), levels=c("5'SS", "3'SS"))
+yMin <- if (opt$test) 0 else 0.7
 ggplot(dfSummary, aes(grp, meanUsage, fill=ss)) +
   geom_col(color="black") +
   geom_errorbar(aes(ymin=meanUsage-seUsage, ymax=meanUsage+seUsage), width=0.2) +
-  coord_cartesian(ylim=c(0.7, 1)) +
+  coord_cartesian(ylim=c(yMin, 1)) +
   geom_signif(comparisons=list(c("Sensitive 5'SS", "Resistant 5'SS"),
                                c("Sensitive 3'SS", "Resistant 3'SS")),
               annotation="***",
@@ -106,7 +141,11 @@ ggplot(dfSummary, aes(grp, meanUsage, fill=ss)) +
 ggsave(file.path("..", "plots", "usagePlotRBP.pdf"))
 
 ## Incorporate amiloride usage data
-amiloUsageLst <- readRDS(file.path("..", "data", "amilo_usage.rds"))
+amiloUsageLst <- if (opt$test) {
+  readRDS(file.path("..", "example_data", "amilo_usage.rds"))
+} else {
+  readRDS(file.path("..", "data", "amilo_usage.rds"))
+}
 amiloUsage <- data.frame(
   grp=c("Sensitive 5'SS",
         "Resistant 5'SS",
