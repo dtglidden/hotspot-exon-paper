d9bfbaa28605bd0872d28850d82a3c9d7e293a53
David Glidden <dtglidden@gmail.com>
2019-12-13 14:38:24 -0500

    Updating how GBM is trained to apply to all exonic variants

--- scripts/train_model.R
+++ scripts/train_model.R
@@ -2,8 +2,9 @@
 ## Train GBM model on MaPSy alleles and store it
 
 suppressPackageStartupMessages({
-  source(file.path("..", "lib", "feature_gen.R"))
+  source(file.path("..", "lib", "feature_gen.R"), chdir=T)
   library(sqldf)
+  library(dplyr)
   library(gbm)
   library(parallel)
   library(ROCR)
@@ -11,73 +12,45 @@ suppressPackageStartupMessages({
 
 nCores <- detectCores()
 
+set.seed(1337)
+
 exsWithSSUsage <- QueryExonsWithSSUsage(as.GRanges=T)
 
-df <- read.csv(file.path("..", "data", "pMaster_020615_1.csv"))
-df <- df[grepl("^HGMD", df$classifier), ]
-
-gr <- GRanges(paste(sub("-", ":", df$ex_hg19ann), df$strand, sep=":"))
-start(gr) <- start(gr) + 1
-gr$hek_ws <- df$hek_ws
-gr$hek_wu <- df$hek_wu
-gr$hek_ms <- df$hek_ms
-gr$hek_mu <- df$hek_mu
-gr$mutation_base_change <- as.factor(paste0(df$allele_w, df$allele_m))
-gr$w5score <- df$w5score
-gr$w3score <- df$w3score
-gr$m5score <- df$m5score
-gr$m3score <- df$m3score
-gr$mwdif_5score <- df$mwdif_5score
-gr$mwdif_3score <- df$mwdif_3score
-gr <- AddExonId(gr)
-
+gr <- readRDS(file.path("..", "data", "mapsy_features_gr.rds"))
+## Temp fix: change booleans to integers in order to train ML models
+gr$isWtPaired <- as.integer(gr$isWtPaired)
+gr$isMutPaired <- as.integer(gr$isMutPaired)
 ## Have to merge dataframes via sqldf because the regular merge function affects the ordering.
 ## This way, the exon IDs are in the same order as those in 'gr'
 grMcols <- as.data.frame(mcols(gr))
-usageMcols <- as.data.frame(mcols(exsWithSSUsage))
-mcols(gr) <- sqldf(paste("SELECT grMcols.*, ss5usage, ss3usage, chasin_ese_density, chasin_ess_density",
-                         "FROM grMcols LEFT JOIN usageMcols",
-                         "ON grMcols.exon_id=usageMcols.exon_id;"))
+exomeMcols <- as.data.frame(mcols(exsWithSSUsage))
+exomeMcolNames <- paste(c("ss5seq", "ss5score", "ss3seq", "ss3score",
+                         "ss5usage", "ss3usage",
+                         "chasin_ese_density", "chasin_ess_density",
+                         "ei"), collapse=", ")
+mcols(gr) <- sqldf(sprintf(paste("SELECT grMcols.*, %s",
+                                 "FROM grMcols LEFT JOIN exomeMcols",
+                                 "ON grMcols.exon_id=exomeMcols.exon_id;"), exomeMcolNames))
 gr <- gr[complete.cases(mcols(gr))]
 
-nRows <- length(gr)
-
-#allelic_skew <- AllelicSkew(gr$hek_ws, gr$hek_wu, gr$hek_ms, gr$hek_mu)
-#affects_splicing <- as.factor(allelic_skew >= log2(1.5)) # p-value?
-#gr$affects_splicing <- affects_splicing
-#responseGBM <- as.integer(affects_splicing) - 1
-splitIdx <- floor(0.7 * nRows)
-trainIndices <- 1:splitIdx
-testIndices <- (splitIdx+1):nRows
-
 allelic_skew <- AllelicSkew(gr$hek_ws, gr$hek_wu, gr$hek_ms, gr$hek_mu)
-affects_splicing <- as.factor(allelic_skew >= log2(1.5)) # p-value?
+affects_splicing <- as.factor(allelic_skew >= log2(1.5))
 gr$affects_splicing <- affects_splicing
-response <- affects_splicing[trainIndices]
+response <- affects_splicing
 responseGBM <- as.integer(response) - 1
 
-mlDf <- as.data.frame(mcols(gr))[, c(
-#  "hek_ws",
-#  "hek_wu",
-  "mutation_base_change",
-  "w5score",
-  "w3score",
-  "m5score",
-  "m3score",
-  "mwdif_5score",
-  "mwdif_3score",
-  "ss5usage",
-  "ss3usage",
-  "chasin_ese_density",
-  "chasin_ess_density"
-)]
+## Using MaPSy count severely limits the number of exons we can use for predictions,
+## so we will exclude it to make predictions for all possible exonic variants.
+## Splice site usage also restricts the number of exons to a lesser degree, but
+## we will keep it in this model
+featureDf <- read.csv(file.path("..", "data", "feature_table.csv"), stringsAsFactors=F) %>%
+  filter(!feature %in% c("hek_wu", "hek_ws"))
 
-gbmDf <- cbind(responseGBM, mlDf[trainIndices, ])
-test <- mlDf[testIndices, ]
+mlDf <- cbind(responseGBM, as.data.frame(mcols(gr))[, featureDf$feature])
 
 ## Most function arguments were taken from the example on the man page for 'gbm'
 model <- gbm(responseGBM ~ .,             # formula
-              data=gbmDf,                  # dataset
+              data=mlDf,                  # dataset
               #var.monotone=c(0,0,0,0,0,0), # -1: monotone decrease,
                                           # +1: monotone increase,
                                           #  0: no monotone restrictions
@@ -95,11 +68,5 @@ model <- gbm(responseGBM ~ .,             # formula
               verbose=F,                   # don't print out progress
               n.cores=nCores)
 best.iter <- gbm.perf(model, method="cv", plot.it=F)
-predNums <- predict(model, test, n.trees=best.iter, type="response")
-
-realityLabels <- affects_splicing[testIndices]
-rocrPred <- prediction(predNums, realityLabels)
-auc <- performance(rocrPred, "auc")
-myRoc <- performance(rocrPred, "tpr", "fpr")
 
 saveRDS(list(model=model, n.trees=best.iter), file.path("..", "data", "ml_model_no_hek.rds"))
